cdd87fd5c4c81f96147da1696eb603da1e454674
Olga Dubavets <o.dubavets@gmail.com>
2025-02-01 22:06:28 +0100

    feat: change App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import { Component } from 'react';
+import { CardList } from './components/cardList/CardList';
+import { Search } from './components/search/Search';
+import { ErrorButton } from './components/errorButton/ErrorButton';
 
 class App extends Component {
+  state: { searchValue: string } = {
+    searchValue: localStorage.getItem('searchValue') ?? '',
+  };
+
+  handleSearchChange = (value: string): void => {
+    localStorage.setItem('searchValue', value);
+    this.setState({ searchValue: value });
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Search
+          handleClickSearch={this.handleSearchChange}
+          defaultSearchValue={this.state.searchValue}
+        />
+        <CardList searchValue={this.state.searchValue} />
+        <ErrorButton />
+      </div>
+    );
   }
 }
 
