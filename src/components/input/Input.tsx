7c07d5dadd484755d3204d058170fa01c43e5e82
Olga Dubavets <o.dubavets@gmail.com>
2025-02-06 11:20:24 +0100

    feat: change Input component

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,8 +1,11 @@
-import { InputHTMLAttributes, ReactElement } from 'react';
+import { InputHTMLAttributes, ReactElement, forwardRef } from 'react';
 import s from './style.module.css';
 
-export const Input = ({
-  ...rest
-}: InputHTMLAttributes<HTMLInputElement>): ReactElement => {
-  return <input className={s.input} {...rest} />;
-};
+export const Input = forwardRef<
+  HTMLInputElement,
+  InputHTMLAttributes<HTMLInputElement>
+>(({ ...rest }, ref): ReactElement => {
+  return <input className={s.input} ref={ref} {...rest} />;
+});
+
+Input.displayName = 'Input';
