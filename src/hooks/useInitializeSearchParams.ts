e9223957558438725980c0607ca9b6a831db6cb1
Olga Dubavets <o.dubavets@gmail.com>
2025-02-06 11:16:22 +0100

    feat: add localStorage.getItem logic to custom hook

---
+++ src/hooks/useInitializeSearchParams.ts
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router';
+
+export const useInitializeSearchParams = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const savedSearchValue = localStorage.getItem('searchValue');
+    if (savedSearchValue && !searchParams.get('search')) {
+      setSearchParams({ search: savedSearchValue });
+    }
+  }, []);
+};
