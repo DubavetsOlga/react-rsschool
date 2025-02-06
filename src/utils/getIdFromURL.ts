55a70dea0e78433d85e4eb06a6251bea881bc5d7
Olga Dubavets <o.dubavets@gmail.com>
2025-02-06 11:18:47 +0100

    feat: add getIdFromURL util function

---
+++ src/utils/getIdFromURL.ts
@@ -0,0 +1,6 @@
+export const getIdFromUrl = (url: string) => {
+  const trimmedUrl = url.endsWith('/') ? url.slice(0, -1) : url;
+  const segments = trimmedUrl.split('/');
+
+  return segments[segments.length - 1];
+};
